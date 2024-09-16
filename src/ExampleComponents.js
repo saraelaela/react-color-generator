6414a3933157dcbc067395003d6ab0bf1364b3ae
saraelaela <saraelabed@outlook.de>
2024-09-16 13:47:10 +0200

    setting up the project

---
+++ src/ExampleComponents.js
@@ -0,0 +1,16 @@
+export default function ExampleComponents(props) {
+  return (
+    <>
+      <h1>The color generator</h1>
+      <input>{props.colorHexCode}</input>
+    </>
+  );
+};
+
+
+export default function ExampleCounts() {
+const [count, setCount] = useState(0);
+  return (
+
+  );
+};
