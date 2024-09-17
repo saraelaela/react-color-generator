98c366437149591b1d45d27a140aca4aebb59679
saraelaela <saraelabed@outlook.de>
2024-09-17 21:20:03 +0200

    added HEX color and rgba, Input field shows HEX color code and allows editing of HEX Color code

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
+import {
+  ColourCode,
+  hexToHsla,
+  hexToRgba,
+  hslaToHex,
+  hslaToHsl,
+  hslaToRgba,
+  hslToHsla,
+  rgbaToHex,
+  rgbaToHsla,
+  rgbaToRgb,
+  rgbToRgba,
+} from 'colour-codes';
 import { ColorGenerator } from 'hex-color-randomizer';
+import { GetColorName } from 'hex-color-to-color-name';
 import React, { useState } from 'react';
-import { HexColorPicker } from 'react-colorful';
 import ExampleComponents from './ExampleComponents';
 
 export default function App() {
   // line 1, defining states
   const generateRandomColor = ColorGenerator.generateRandomHexColor();
   const [color, setColor] = useState(generateRandomColor);
+  const rgbColor = hexToRgba(color).rgbaString;
+  const colorName = GetColorName(color);
 
   // line 2, executing the defined code
   return (
     <>
-      <div>
-        <div>Info 1</div>
-        <div>Info 2</div>
-        <div>Info 3</div>
+      <div style={{ display: 'flex', padding: 10 }}>
+        <div style={{ flexGrow: 3 }}>
+          Sara El Abed <br /> Cohort Fall 2024
+        </div>
+        <div style={{ flexGrow: 3 }}>
+          HEX {color} <br /> {rgbColor} {colorName}
+        </div>
+        <div style={{ flexGrow: 3, textAlign: 'right' }}>
+          <button
+            onClick={() => setColor(ColorGenerator.generateRandomHexColor())}
+          >
+            random
+          </button>
+        </div>
       </div>
-      <button onClick={() => setColor(ColorGenerator.generateRandomHexColor())}>
-        random
-      </button>
 
-      <ExampleComponents color={color} setColor={setColor} />
+      <ExampleComponents
+        color={color}
+        setColor={setColor}
+        colorName={colorName}
+      />
     </>
-  ); // 1. create a state component inside the parent
+  );
 }
-
-// export default function App() {
-//   const [color, setColor] = useState('#b32aa9');
-//   const [newColor, setNewColor] = useState({ color });
-//   };
-
-//   return (
-//     <div className="App">
-//       <HexColorPicker color={color} onChange={setColor} />
-
-//       <input
-//         value={color}
-//         onChange={(event) =>} // Use onChange to capture input changes
-//       />
-//       <div>{newColor}</div>
-
-//       <div className="buttons">
-//         <button onClick={() => setColor('#c6ad23')}>Choose gold</button>
-//         <button onClick={() => setColor('#556b2f')}>Choose green</button>
-//         <button onClick={() => setColor('#207bd7')}>Choose blue</button>
-//       </div>
-//     </div>
-//   );
-// }
